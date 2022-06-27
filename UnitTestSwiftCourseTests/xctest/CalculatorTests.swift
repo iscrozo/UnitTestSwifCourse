b511684f1523e49f085ba391518acab379720b46
“Sebastian <iscrozo@gmail.com>
2022-06-27 12:12:20 -0500

    mas test y error

--- UnitTestSwiftCourseTests/xctest/CalculatorTests.swift
+++ UnitTestSwiftCourseTests/xctest/CalculatorTests.swift
@@ -37,10 +37,49 @@ class CalculatorTests: XCTestCase {
         resultadoActual = sut.add(n1: 10, n2: 20)
         // 3- assert
         XCTAssertEqual(resultadoActual, resultadoEsperado)
-        
+    }
+    
+    func testAddBDD() {
+        print("call testAddBDD() CalculatorTests")
+        // 1- Given
+        let resultadoEsperado = 30
+        var resultadoActual: Int
+        // 2- When
+        resultadoActual = sut.add(n1: 10, n2: 20)
+        // 3- Then
+        XCTAssertEqual(resultadoActual, resultadoEsperado)
+    }
+    
+    func testAdd() {
+        print("call testAdd() CalculatorTests")
+        // 3- Then
+        XCTAssertEqual(sut.add(n1: 10, n2: 20), 30)
     }
 
+    func testAssertTypes() {
+        print("call testAssertTypes() CalculatorTests")
+        XCTAssertTrue( 1 == 1 )
+        XCTAssertFalse( 1 == 2 )
+        XCTAssertNil( nil )
+        XCTAssertNotNil( sut )
+        XCTAssertEqual("camilo", "camilo", "equal names")
+        XCTAssertGreaterThan(10, 1, "Necesito que sea mayor")
+    }
     
+    func testDivide(){
+        print("call testDivide() CalculatorTests")
+        XCTAssertEqual(2, sut.divide(n1: 4, n2: 2), "Tiene que ser la mitad")
+    }
+    
+    func testDivideByZero(){
+        print("call testDivideByZero() CalculatorTests")
+        XCTAssertEqual(2, try sut.divideByZero(n1: 4, n2: 2))
+        XCTAssertThrowsError(try sut.divideByZero(n1: 4, n2: 0))
+    }
+    
+    func testDisable() {
+        print("call testDisable() CalculatorTests")
+    }
 }
 
 
